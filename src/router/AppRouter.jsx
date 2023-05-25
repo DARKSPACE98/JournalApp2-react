6404f5973852507706263a5526ca0353d1df24b6
DARKSPACE98 <enriquechuyin2019@gmail.com>
2023-05-25 00:02:13 -0700

    Se agrega el CheckingAuth y cambios en el AppRouter para las rutas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,53 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import { AuthRoutes } from "../auth/routes/AuthRoutes"
 import { JournalRoutes } from "../journal/routes/JournalRoutes"
+import { useDispatch, useSelector } from "react-redux"
+import { CheckingAuth } from "../ui/";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { FirebaseAuth } from "../firebase/config";
+import { login, logout } from "../strore/auth";
 
 export const AppRouter = () => {
+
+const { status }  = useSelector( state => state.auth);
+
+const dispatch = useDispatch();
+
+useEffect(() => {
+  
+  onAuthStateChanged( FirebaseAuth, async( user ) =>{
+    if ( !user ) return dispatch( logout() );
+    const { uid, email, displayName, photoURL} = user;
+    dispatch( login ({ uid, email, displayName, photoURL }) );
+  })
+  
+  
+}, [])
+
+
+if ( status === 'checking' ){
+  return <CheckingAuth />
+}
+
+
+
   return (
     <Routes>
+
+      {
+        (status === 'authenticated')
+        ?//Si 
+        <Route  path="/*" element={ <JournalRoutes/> } />
+        ://Si no entonces muestra
         <Route path="/auth/*" element={ <AuthRoutes /> } />
+      }
+
+        <Route path='/*' element={ < Navigate to='/auth/login'/> } />
+        {/* <Route path="/auth/*" element={ <AuthRoutes /> } /> */}
             {/* Login y Registro */}
             
-        <Route  path="/*" element={ <JournalRoutes/> } />
+        {/* <Route  path="/*" element={ <JournalRoutes/> } /> */}
             {/* JournalApp */}
     </Routes>
   )
--- src/strore/auth/authSlice.js
+++ src/strore/auth/authSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const authSlice = createSlice({
     name: 'auth',
     initialState: {
-        status: 'not-authenticated', //'checking', 'nat-authenticated', 'authenticated'
+        status: 'checking', //'checking', 'nat-authenticated', 'authenticated'
         uid: null, 
         email: null,
         displayName: null,
