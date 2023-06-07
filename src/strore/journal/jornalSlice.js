906e9a129c5b82af1bea9d519401596fc30f0a6d
DARKSPACE98 <enriquechuyin2019@gmail.com>
2023-06-06 23:05:33 -0700

    Se agresa la vista para las nuevas notas en la barra lateral

--- src/strore/journal/jornalSlice.js
+++ src/strore/journal/jornalSlice.js
@@ -34,7 +34,7 @@ export const journalSlice = createSlice({
 
         setNotes: (state, action) => {
             state.notes = action.payload;
-
+            
         },
 
         setSaving:(state) => {
